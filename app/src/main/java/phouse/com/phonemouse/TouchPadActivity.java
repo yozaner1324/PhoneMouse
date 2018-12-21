672994770a0d51d46f589ea7ec6d6fbcf7ed27b5
Patrick Johnson <kcirtap1324@gmail.com>
2018-12-20 19:02:31 -0800

    Put controls into fragment

--- app/src/main/java/phouse/com/phonemouse/TouchPadActivity.java
+++ app/src/main/java/phouse/com/phonemouse/TouchPadActivity.java
@@ -4,9 +4,6 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 public class TouchPadActivity extends AppCompatActivity
 {
@@ -26,53 +23,8 @@ public class TouchPadActivity extends AppCompatActivity
         Connection conn = (Connection) getIntent().getExtras().get("conn");
         commander = new MouseCommander(conn);
 
-        // left click
-        Button left = findViewById(R.id.leftClick);
-        left.setOnTouchListener(new View.OnTouchListener()
-        {
-            @Override
-            public boolean onTouch(View v, MotionEvent event)
-            {
-                switch(event.getAction())
-                {
-                    case MotionEvent.ACTION_DOWN:
-                        commander.leftClick();
-                        break;
-
-                    case MotionEvent.ACTION_UP:
-                        commander.releaseLeftClick();
-                        break;
-                }
-
-                return false;
-            }
-
-        });
-
-        // scrolling
-        ImageView scroll = findViewById(R.id.scroll);
-        scroll.setOnTouchListener(new View.OnTouchListener()
-        {
-            @Override
-            public boolean onTouch(View v, MotionEvent event)
-            {int index = event.getActionIndex();
-                int pointerId = event.getPointerId(index);
-                if(event.getAction() == MotionEvent.ACTION_MOVE)
-                {
-                    int notches = Math.round(event.getY(pointerId) - lastY)/2;
-                    if(notches != 0)
-                    {
-                        commander.scroll(notches);
-                        lastY = event.getY(pointerId);
-                    }
-                }
-                else if (event.getAction() == MotionEvent.ACTION_DOWN)
-                {
-                    lastY = event.getY(pointerId);
-                }
-                return true;
-            }
-        });
+        ControlFragment fragment = (ControlFragment) getSupportFragmentManager().findFragmentById(R.id.frag);
+        fragment.setup(commander);
     }
 
     // touch pad
@@ -99,12 +51,6 @@ public class TouchPadActivity extends AppCompatActivity
                 break;
         }
         return true;
-    }
-
-    // right click
-    public void rightClick(View view)
-    {
-        commander.rightClick();
     }
 
     @Override
